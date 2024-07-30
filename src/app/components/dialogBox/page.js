e1eb0b66e2d24a3e9aef58eee237ee6ba6c07c06
ujjannoormustafa <ujjannoormustafa123@gmail.com>
2024-07-30 19:05:21 +0500

    fix

--- src/app/components/dialogBox/page.js
+++ src/app/components/dialogBox/page.js
@@ -2,8 +2,10 @@
 import React from "react";
 import { useState } from "react";
 import { Button, Modal } from 'antd';
-const page = () => {
+
+const Page = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+
     const showModal = () => {
         setIsModalOpen(true);
     };
@@ -21,15 +23,14 @@ const page = () => {
             </Button>
             <Modal title="Select Week" open={isModalOpen} centered onOk={handleOk} onCancel={handleCancel}>
                 <div className="flex justify-around my-4 py-4 items-center space-4">
-                    <div className="bg-gray-200  hover:bg-gray-300 cursor-pointer rounded  p-1 lg:px-6 py-2">week 1</div>
-                    <div className="bg-gray-200  hover:bg-gray-300 cursor-pointer rounded  p-1 lg:px-6 py-2">week 2</div>
-                    <div className="bg-gray-200  hover:bg-gray-300 cursor-pointer rounded  p-1 lg:px-6 py-2">week 3</div>
-                    <div className="bg-gray-200  hover:bg-gray-300 cursor-pointer rounded  p-1 lg:px-6 py-2">week 4</div>
-
+                    <div className="bg-gray-200 hover:bg-gray-300 cursor-pointer rounded p-1 lg:px-6 py-2">week 1</div>
+                    <div className="bg-gray-200 hover:bg-gray-300 cursor-pointer rounded p-1 lg:px-6 py-2">week 2</div>
+                    <div className="bg-gray-200 hover:bg-gray-300 cursor-pointer rounded p-1 lg:px-6 py-2">week 3</div>
+                    <div className="bg-gray-200 hover:bg-gray-300 cursor-pointer rounded p-1 lg:px-6 py-2">week 4</div>
                 </div>
             </Modal>
         </>
-    )
+    );
 }
 
-export default page
+export default Page;
--- src/app/components/products/page.js
+++ src/app/components/products/page.js
@@ -115,7 +115,7 @@ const GetData = () => {
 
                                 data.map((recipe) => (
 
-                                    <div className="bg-white shadow-[0_4px_12px_-5px_rgba(0,0,0,0.4)] w-full max-w-sm rounded-lg overflow-hidden mx-auto font-[sans-serif] mt-4">
+                                    <div key={recipe.id} className="bg-white shadow-[0_4px_12px_-5px_rgba(0,0,0,0.4)] w-full max-w-sm rounded-lg overflow-hidden mx-auto font-[sans-serif] mt-4">
                                         <div className="min-h-[206px]">
                                             <img src={recipe.image} className="w-full" />
                                         </div>
